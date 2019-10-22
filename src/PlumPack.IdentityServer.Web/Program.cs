7dc9b3207f8987d6664e28dc8644e1774f03c5da
Paul Knopf <pauldotknopf@gmail.com>
2019-10-21 23:43:14 -0400

    Moved a lot of Program.Main to shared lib.

--- src/PlumPack.IdentityServer.Web/Program.cs
+++ src/PlumPack.IdentityServer.Web/Program.cs
@@ -1,110 +1,46 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
-using CommandLine;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using PlumPack.Infrastructure.Data;
-using PlumPack.Infrastructure.Migrations;
 using Serilog;
 
 namespace PlumPack.IdentityServer.Web
 {
     public class Program
     {
-        public class Options
-        {
-            [Option('h', "http-port", Default = 5000)]
-            public int HttpPort { get; set; }
-        }
-        
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(new ConfigurationBuilder()
-                    .AddJsonFile("logging.json", true)
-                    .Build())
-                .WriteTo.Console()
-                .CreateLogger();
-            
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(o =>
+            PlumPack.Web.Main.Program.Main<Startup>(5000, "/etc/plumpack/identity-server/", args, async host =>
+            {
+                using (var scope = host.Services.CreateScope())
                 {
-                    var host = CreateHostBuilder(o).Build();
+                    var normalizer = scope.ServiceProvider.GetRequiredService<ILookupNormalizer>();
+                    var userStore = scope.ServiceProvider.GetRequiredService<IUserStore<User>>();
+                    var userEmailStore = scope.ServiceProvider.GetRequiredService<IUserEmailStore<User>>();
+                    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 
-                    using (var scope = host.Services.CreateScope())
-                    {
-                        scope.ServiceProvider.GetRequiredService<IDataService>().WaitForDbConnection(TimeSpan.FromSeconds(5));
-                        var migrator = scope.ServiceProvider.GetRequiredService<IMigrator>();
-                        migrator.Migrate();
-                    }
-                    
-                    using (var scope = host.Services.CreateScope())
-                    {
-                        var normalizer = scope.ServiceProvider.GetRequiredService<ILookupNormalizer>();
-                        var userStore = scope.ServiceProvider.GetRequiredService<IUserStore<User>>();
-                        var userEmailStore = scope.ServiceProvider.GetRequiredService<IUserEmailStore<User>>();
-                        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-                        
-                        Task.Run(async () =>
-                        {   
-                            var admin = await userStore.FindByNameAsync(normalizer.NormalizeName(User.DefaultUserName), CancellationToken.None);
-                            
-                            if (admin != null)
-                            {
-                                // Admin already exists.
-                                return;
-                            }
-                            
-                            admin = new User();
-                            await userStore.SetUserNameAsync(admin, User.DefaultUserName, CancellationToken.None);
-                            await userEmailStore.SetEmailAsync(admin, User.DefaultEmail, CancellationToken.None);
-                            await userEmailStore.SetEmailConfirmedAsync(admin, true, CancellationToken.None);
-                            var createResult = await userManager.CreateAsync(admin, User.DefaultPassword);
-                            
-                            if (!createResult.Succeeded)
-                            {
-                                Log.Logger.Fatal("Couldn't create the default admin user.");
-                                Environment.Exit(1);
-                            }
-                            
-                        }).GetAwaiter().GetResult();
-                        
-                    }
-                    
-                    host.RunAsync().GetAwaiter().GetResult();
-                });
-        }
+                    var admin = await userStore.FindByNameAsync(normalizer.NormalizeName(User.DefaultUserName),
+                        CancellationToken.None);
 
-        public static IHostBuilder CreateHostBuilder(Options options)
-        {
-            return Host.CreateDefaultBuilder()
-                .ConfigureHostConfiguration(builder =>
-                {
-                    if (File.Exists("/etc/plumpack/identity-server/config.yml"))
+                    if (admin != null)
                     {
-                        builder.AddYamlFile("/etc/plumpack/identity-server/config.yml");
+                        // Admin already exists.
+                        return;
                     }
-                })
-                .ConfigureLogging(config =>
-                {
-                    config.ClearProviders();
-                    config.AddSerilog();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    var urls = $"http://*:{options.HttpPort}/";
-                    Log.Logger.Information($"Listening on: {urls}");
-                    webBuilder.UseUrls(urls);
-                    webBuilder.UseStartup<Startup>();
-                });
+
+                    admin = new User();
+                    await userStore.SetUserNameAsync(admin, User.DefaultUserName, CancellationToken.None);
+                    await userEmailStore.SetEmailAsync(admin, User.DefaultEmail, CancellationToken.None);
+                    await userEmailStore.SetEmailConfirmedAsync(admin, true, CancellationToken.None);
+                    var createResult = await userManager.CreateAsync(admin, User.DefaultPassword);
+
+                    if (!createResult.Succeeded)
+                    {
+                        Log.Logger.Fatal("Couldn't create the default admin user.");
+                        Environment.Exit(1);
+                    }
+                }
+            });
         }
     }
 }
